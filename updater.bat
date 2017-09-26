bfbc78fd1d54a28469585fd9b0646dd58dd2a44c
moesora <soutear@gmail.com>
2017-09-26 18:41:19 +0800

    Update updater.bat

--- updater.bat
+++ updater.bat
@@ -1,5 +1,5 @@
 @echo off
 cd /d %~dp0
-wget -q -N "https://coding.net/u/scaffrey/p/hosts/git/raw/master/hosts-files/hosts" -o hosts
-copy /y "hosts" "%SystemRoot%\System32\drivers\etc\hosts"
+wget -q -N https://coding.net/u/scaffrey/p/hosts/git/raw/master/hosts-files/hosts
+copy /y hosts %SystemRoot%\System32\drivers\etc\hosts
 ipconfig /flushdns
